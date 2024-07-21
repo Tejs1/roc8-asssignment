38c7de1f336fc3e7ef78cf01a31b385eed5e3169
Tejs1 <tejsrelax@gmail.com>
2024-07-21 10:13:14 +0000

    refactor: Update site.ts to use TypeScript type for SiteConfig

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,12 +1,22 @@
-import { SiteConfig } from "@/types"
+export type SiteConfig = {
+  name: string;
+  description: string;
+  url: string;
+  ogImage: string;
+  links: {
+    twitter: string;
+    github: string;
+  };
+};
+
 export const siteConfig: SiteConfig = {
-	name: "Neucomm",
-	description:
-		"An open source application built using the new router, server components and everything new in Next.js 13.",
-	url: "https://neucom.vercel.app",
-	ogImage: "https://neucom.vercel.app/og.jpg",
-	links: {
-		twitter: "https://twitter.com/tejsrelax",
-		github: "https://github.com/Tejs1/neucomm",
-	},
-}
+  name: "Neucomm",
+  description:
+    "An open source application built using the new router, server components and everything new in Next.js 13.",
+  url: "https://neucom.vercel.app",
+  ogImage: "https://neucom.vercel.app/og.jpg",
+  links: {
+    twitter: "https://twitter.com/tejsrelax",
+    github: "https://github.com/Tejs1/neucomm",
+  },
+};
