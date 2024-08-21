f2cab41156cdd8f946fc33a14e14ca599434053c
Tejs1 <tejsrelax@gmail.com>
2024-08-21 18:42:18 +0000

    refactor:  fix  paragraph tag

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,7 +27,7 @@ export default async function IndexPage() {
           <div className="space-x-4">
             <Link
               href={{
-                pathname: '/sign-in',
+                pathname: "/sign-in",
               }}
               className={cn(buttonVariants({ size: "lg" }))}
             >
@@ -52,12 +52,11 @@ export default async function IndexPage() {
           <h2 className="font-heading text-3xl leading-[1.1] sm:text-3xl md:text-6xl">
             Features
           </h2>
-          <p className="max-w-[85%]  text-muted-foreground sm:text-lg sm:leading-7">
-            <p className="max-w-[85%]  text-muted-foreground sm:text-lg sm:leading-7">
-              This project is an experiment to see how a modern app, with features
-              like auth, subscriptions, API routes, and static pages would work in
-              Next.js 14 app dir.
-            </p>
+          <p className="max-w-[85%] text-muted-foreground sm:text-lg sm:leading-7">
+            This project is an experiment to see how a modern app, with features
+            like auth, subscriptions, API routes, and static pages would work in
+            Next.js 14 app dir.
+          </p>
         </div>
         <div className="mx-auto grid justify-center gap-4 sm:grid-cols-2 md:max-w-[64rem] md:grid-cols-3">
           <div className="relative overflow-hidden rounded-lg border bg-background p-2">
